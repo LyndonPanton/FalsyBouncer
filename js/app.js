473d3132c8d1fafd9af7411362a6b052f64d26a5
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-28 18:51:42 +0100

    Added logic for bouncing falsy values from array

--- js/app.js
+++ js/app.js
@@ -3,8 +3,22 @@
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	function bouncer(input) {
+		if (input.indexOf(", ") === -1) {
+			return "Literals must be separated with this: \", \"";
+		} else {
+			let bounced = input.split(", ").map(function(value) {
+				return Boolean(value);
+			});
+
+			return bounced;
+		}
+	}
+
 	let form = document.getElementById("form");
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
+
+		bouncer(this.children[0].value);
 	});
 };
